a1a8a730208a1f063557f463cc99c476766283c7
wenxin8 <62378308+wenxin8@users.noreply.github.com>
2023-04-13 20:02:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
-options.headless = True
+options.add_argument('-headless')
 driver = webdriver.Firefox(options=options)
 driver.get("https://www.google.com")
 
