09b9c3f25028d00d333023e5300c5b925012cb61
wenxin8 <62378308+wenxin8@users.noreply.github.com>
2023-04-13 19:32:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,18 @@
-# 导入必要的库
-from selenium import webdriver
-import time
-import base64
-import re
-from pyzbar.pyzbar import decode
+import requests
 from PIL import Image
+from io import BytesIO
 
-# 创建浏览器对象
-driver = webdriver.Chrome()
+# 获取网页内容
+url = "https://www.youtube.com/watch?v=RCp9hnp7r6Q"
+response = requests.get(url)
 
-# 打开指定的网页
-url = 'https://www.youtube.com/watch?v=RCp9hnp7r6Q'
-driver.get(url)
+# 将网页内容转为二进制流
+stream = BytesIO(response.content)
 
-# 暂停等待页面加载完成
-time.sleep(5)
+# 打开图片并创建 Image 对象
+img = Image.open(stream)
 
-# 定位二维码元素并截图
-qrcode_element = driver.find_element_by_xpath(
-    '//img[contains(@class, "qr-image") and contains(@src, "data:image/png;base64")]'
-)
-qrcode_src = qrcode_element.get_attribute('src')
-qrcode_data = re.sub('^data:image/.+;base64,', '', qrcode_src)
-qrcode_bytes = base64.b64decode(qrcode_data)
-with open('qrcode.png', 'wb') as f:
-    f.write(qrcode_bytes)
+# 裁剪、缩放等操作（可选）
 
-# 解析二维码图片并显示结果
-qrcode_image = Image.open('qrcode.png')
-qrcode_info = decode(qrcode_image)[0].data.decode('utf-8')
-print(qrcode_info)
-
-# 关闭浏览器
-driver.quit()
+# 保存图片
+img.save("screenshot.png")
