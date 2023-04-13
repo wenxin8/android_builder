ddb7e05e05ad3378d1526031622ca9495c3879c3
wenxin8 <62378308+wenxin8@users.noreply.github.com>
2023-04-13 22:10:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from PIL import Image
 from pyzbar.pyzbar import decode
