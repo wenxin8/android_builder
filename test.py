bde7dac31955961ea0622254a457be11e57baf8b
wenxin8 <62378308+wenxin8@users.noreply.github.com>
2023-04-13 20:07:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-
+import time
 options = Options()
 options.add_argument('-headless')
 driver = webdriver.Firefox(options=options)
 driver.get("https://www.google.com")
 
-
+time.sleep(8)
 driver.save_screenshot('screenshot.png')
 
 # 关闭浏览器并退出
